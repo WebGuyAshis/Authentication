c35f4bb42e22f35a669588dcd70c7f68a7a8f981
Ashis <developer.ashis99@gmail.com>
2023-08-21 23:08:02 +0530

    Chnaged redis connect

--- config/redis.js
+++ config/redis.js
@@ -1,16 +1,34 @@
+// const Redis = require("ioredis");
+
+// const redis = new Redis({
+//     host: "127.0.0.1",
+//     port: 6379
+// })
+
+// redis.on('connect', ()=>{
+//     console.log("Successfully Connected to Redis!");
+// })
+
+// redis.on('error', (error)=>{
+//     console.log("Error Connecting to the Redis Server!",error);
+// })
+
+// module.exports = redis;
+
 const Redis = require("ioredis");
 
 const redis = new Redis({
-    host: "127.0.0.1",
-    port: 6379
-})
+    host: "singapore-redis.render.com", // Correct hostname
+    port: 6379,
+    password: "zc4KOc4h3qwx9ES8lgWgKg9dWfFwdLE9" // Auth password
+});
 
-redis.on('connect', ()=>{
+redis.once('connect', () => {
     console.log("Successfully Connected to Redis!");
-})
+});
 
-redis.on('error', (error)=>{
-    console.log("Error Connecting to the Redis Server!",error);
-})
+redis.once('error', (error) => {
+    console.log("Error Connecting to the Redis Server!", error);
+});
 
 module.exports = redis;
